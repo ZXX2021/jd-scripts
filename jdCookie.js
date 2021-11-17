acbfc9c27f7aa0065f51efa5a6abff75a76c3502
ZXX2021 <93639339+ZXX2021@users.noreply.github.com>
2021-11-17 12:20:05 +0800

    Update jdCookie.js

--- jdCookie.js
+++ jdCookie.js
@@ -4,11 +4,7 @@
 //此处填写京东账号cookie。
 //注：github action用户cookie填写到Settings-Secrets里面，新增JD_COOKIE，多个账号的cookie使用`&`隔开或者换行
 let CookieJDs = [
-        // "pt_key=app_openAAJhg75jADDtxGDiR3FFsIRaro6DPPzsYClV358huEQxLmMCntIZ-XlBBY8iY6ot3Epyg3sMB-o;pt_pin=wdNnyZxalapKVOH;pt_token=ci39o610;remark=qiuju1;",
-        // "pt_key=app_openAAJhg75kADDDhs9C6KPKRiUIDaSfW7QICaEEbz7eErQxymeOhR0KASnYbWUEcdWeY77YYZjulm0;pt_pin=jd_KsbLBWLStKZi;pt_token=mib56ax6;remark=qiuju2;",
-        'pt_key=app_openAAJhg75jADBjNs4Hoc4sxMGDIsmAxQRQ1_CkOOhspKWQQKVCrhKeE8r2Abv10ul8uElxeuqK1SE;pt_pin=jd_54cf0ebef60bf;pt_token=d5z3bz5d;remark=qiuhua;', //账号二ck,例:pt_key=XXX;pt_pin=XXX;如有更多,依次类推
-        'pt_key=app_openAAJhg75nADDT6CWF3cr_yOnVQ4bewpfi2vbOn6IAH2OU9UsVJVUpU7ouqXXZIHGvILd59EhyCTM;pt_pin=jd_5a65b02fa6eec;',
-        // 'pt_key=app_openAAJhg75hADAjQCw2QXgfgwLgwbt-UpN3KZnl0z4AASmmDy54e1Z_jUOqI3RqDjI7ycY7CKW8YCM;pt_pin=jd_kYbLgxfSQKik;'
+        ""
     ]
     // 判断github action里面是否有京东ck
 if (process.env.JD_COOKIE) {
